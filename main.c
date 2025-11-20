5c01f7a08b8a63b7cb5585c0154c7e1bd748f7ce
AdmiralKeso <cas.hagenaar@gmail.com>
2025-11-20 18:12:31 +0100

    Add: Variables

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+int playGame() {
+    int computerChoice;
+    int playerChoice;
+    int playerScore = 0;
+    int computerScore = 0;
+    int message;
 int main(int argc, char const *argv[])
 {
     playGame();
